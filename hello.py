155621203ef20b64e961170e955684d647d7a26d
Mutinda Mutio <40093051+MutindaMutio@users.noreply.github.com>
2026-09-24 13:46:51 +0100

    created a second_largest() which returns botht the largest and second largest numbers in a list

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
 def largest (numbers):
-    cont = False
     largest = numbers[0]
     for item in numbers:
         if item > largest:
             largest = item
     return largest
 result = largest([-3, -8, -1])
-print(result)       
+print(result) 
+
+def second_largest (numbers):
+    largest = numbers[0]
+    second_largest = numbers[1] # Assumes numbers has at least 2 items, raises IndexError if thats not the case
+    for item in numbers:
+        if item > largest:
+            second_largest = largest
+            largest = item
+        elif item > second_largest:
+            second_largest = item
+    return largest, second_largest
+result = second_largest([-5,-8,6,4])
+print(result)
+         
